1dba281185e957993cc5fa9084b9cdb55e913d87
damienrabelais <damien.boucard@ac-rennes.fr>
2022-11-16 21:44:34 +0100

    ajout css

--- php/premier.php
+++ php/premier.php
@@ -1,7 +1,11 @@
 <html>
+<head>
+<title>Premier</title>
+   <link href="../css/moncss.css" rel="stylesheet" type="text/css"> 
+</head>
 <body>
 <?php
-echo "Mon premier script PHP!";
+echo "<h1>Mon premier script PHP!</h1>";
 ?>
 </body>
 </html>
